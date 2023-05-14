e4639b387f7d7755a414b1c066201f4db3b0033b
thiago <thiagoandre2121@gmail.com>
2023-05-14 18:30:06 -0300

    time per neighborhood

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ from src.features.co2_by_neighbordhood import *
 from src.features.co2_per_busID import *
 from src.features.co2_per_line import *
 from src.features.busID_by_neighbordhood import *
+from src.features.registers_by_neighborhood import *
+from src.features.time_per_neighborhood import *
 
 from src.visualization.bar_graph import *
 from src.visualization.bar_graph_by_days import *
 
-DSTs = filterByTimerange('2022-02-10', '2022-02-13')
+DSTs = filterByTimerange('2022-02-10', '2022-02-10')
 
 #df_means = co2ByNeighbordhood(DSTs)
 #bus_lines = co2PerLine(DSTs)
-bus_ids = busIDByNeighbordhood(DSTs, 'Ricardo de Albuquerque')
+#bus_ids = busIDByNeighbordhood(DSTs, 'Maré')
+#number = registersByNeighborhood(DSTs, 'Ricardo de Albuquerque')
 #barGraphByDays(df_means)
-
+time = timePerNeighborhood(DSTs, 'Engenheiro Leal')
+print(time)
 print('success!')
 
 
---
+++ src/features/time_per_neighborhood.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def timePerNeighborhood(DSTs, neighborhoodName):
+    dstB = [ dstB[['ID', 'NOME']] for dstB in DSTs['dstB']]
+    intervals = [ dstD[['ID', 'INTERVAL']] for dstD in DSTs['dstD']]
+
+    dfs = [pd.merge(dstB[i], intervals[i], on='ID', how='left') for i in range(len(intervals))]
+
+    for df in dfs:
+        df['INTERVAL'] = df['INTERVAL'].dt.total_seconds()
+
+    dataframes_filtrados = []
+
+    for df in dfs:
+        df_filtrado = df[df['NOME'] == neighborhoodName]
+        dataframes_filtrados.append(df_filtrado)
+
+    df_final = pd.concat(dataframes_filtrados)
+    seconds = df_final['INTERVAL'].sum()
+    hours = int((seconds/60/60).round())
+    return hours
